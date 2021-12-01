865f75c34257d51d1dabab328d784e6700b1dccc
Jasmin Mustafic <mustaficjasmin7@gmail.com>
2021-12-01 11:58:41 +0100

    Added arithmetic operations assignment.

---
+++ lekcija-2/arithmetic-operators.js
@@ -0,0 +1,12 @@
+let suma = 12 + 5;      // Sabere brojeve, zatim spremi u varijablu suma
+let razlika = 12 - 5;   // Oduzme brojeve, zatim spremi u varijablu razlika
+let proizvod = 12 * 5;  // Pomnozi brojeve, zatim spremi u varijablu proizvod
+let kolicnik = 12 / 5;  // Podijeli brojeve, zatim spremi u varijablu kolicnik
+let ostatak = 12 % 5;   // Podijeli brojeve, zatim ostatak spremi u varijablu suma
+
+// Ispis ispod
+console.log(suma);
+console.log(razlika);
+console.log(proizvod);
+console.log(kolicnik);
+console.log(ostatak);
