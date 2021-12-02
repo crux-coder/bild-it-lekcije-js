1095eb4fd6ab408481fd588151e55999164f1703
Jasmin Mustafic <mustaficjasmin7@gmail.com>
2021-12-02 10:09:55 +0100

    Renamed files. Added assignment.

---
+++ lekcija_2 - Strings&Operators/assignmnent-operators-primjer.js
@@ -0,0 +1,12 @@
+let a = 7;
+
+a += 5;
+
+console.log(a);
+
+let b = 12;
+let c = 5;
+
+b %= 5;
+
+console.log(b);
---
+++ lekcija_2 - Strings&Operators/assignmnent-operators-zadatak_2.js
@@ -0,0 +1,18 @@
+let a = 12;
+let b = 5;
+
+a %= 5;
+
+console.log(a);
+
+let c = 7;
+
+c *= 5;
+
+console.log(c);
+
+let d = 9;
+
+d /= 3;
+
+console.log(d);
