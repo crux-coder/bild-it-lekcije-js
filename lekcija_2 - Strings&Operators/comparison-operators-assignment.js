7c8bcd16891cf0d3f09ebb660972bb225ae64584
Jasmin Mustafic <mustaficjasmin7@gmail.com>
2021-12-02 09:28:37 +0100

    Added Comparison operators assignment.

---
+++ lekcija_2 - Strings&Operators/comparison-operators-assignment.js
@@ -0,0 +1,15 @@
+let broj1 = 9;
+let broj2 = 2;
+
+let vece = broj1 > broj2;
+let manje = broj1 < broj2;
+let jednako = broj1 == broj2;
+let razlicito = broj1 != broj2;
+let manjeJednako = broj1 <= broj2;
+
+// Ispis ispod
+console.log(vece);
+console.log(manje);
+console.log(jednako);
+console.log(razlicito);
+console.log(manjeJednako);
---
+++ lekcija_2 - Strings&Operators/comparison-operators-example.js
@@ -0,0 +1,12 @@
+console.log(7 == 5);
+
+let poredjenje1 = 7 > 5;
+
+console.log(poredjenje1);
+
+let broj1 = 4;
+let broj2 = 6;
+
+let poredjenje2 = broj1 != broj2;
+
+console.log(poredjenje2);
